511283173a7bcfaecd0ea3267b7a01ac0b6f10b0
Rodrigo José Sandino Salinas <sandinorodrigo@unitec.edu>
2016-05-06 16:25:41 -0600

    Funviona

--- lab3.cpp
+++ lab3.cpp
@@ -4,8 +4,9 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void agregarFicha(int columna, int turno, int temp[6][7]);
+void agregarFicha(int columna, int& turno, int temp[6][7]);
 void imprimirTablero(int[6][7]);
+bool gameOver(int temp[6][7]);
 
 int main(int argc, char*argv[]){
 	const int filas = 6;
@@ -14,7 +15,7 @@ int main(int argc, char*argv[]){
 	int columna;
 	imprimirTablero(tablero);
 	int chance = 1;
-	while(true){	
+	while(gameOver(tablero)){	
 		if(chance == 1){		
 			cout << "Agregar ficha en columna: ";
 			cin >> columna;
@@ -32,13 +33,26 @@ int main(int argc, char*argv[]){
 	return 0;
 }
 
-void agregarFicha(int columna, int turno, int temp[6][7]){
-	for(int i = 5; i > 0; i--){
-		if(temp[i][columna] == 0){
-			temp[i][columna] = turno;
-			break;
-		} else if(temp[i][columna] != 0 && temp[i-1][columna] == 0){
-			temp[i-1][columna] = turno;
+void agregarFicha(int columna, int& turno, int temp[6][7]){
+	for(int i = 5; i > 0; i--){	
+		if (columna >= 0 && columna <= 6){		
+			if (temp[i][columna] == 0){
+				temp[i][columna] = turno;
+				break;
+			} else if (temp[i][columna] != 0 && temp[i-1][columna] == 0){
+				temp[i-1][columna] = turno;
+				break;
+			}
+			if(temp[0][columna] != 0){
+				cout << "No se puede agregar mas fichas en esta columna" << endl;
+				break;
+			}
+		} else {
+			if (turno == 1)
+				turno = 2;
+			else
+				turno = 1;
+			cout << "Pierde turno, columna inexistente" << endl;
 			break;
 		}
 	}
@@ -51,4 +65,13 @@ void imprimirTablero(int temp[6][7]){
 		}
 		cout << endl;
 	}
+}
+
+bool gameOver(int temp[6][7]){
+	for(int i = 0; i < 6; i++){
+		for(int j = 0; j < 7; j++){
+			
+		}
+	}
+	return true;
 }
